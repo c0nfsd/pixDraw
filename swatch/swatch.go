23ca05e32dec01698109fa1c3e5a6fa4ae4d18fa
c0nfsd <pathakmilindd@gmail.com>
2022-08-13 23:45:12 +0530

    swatch color

--- swatch/swatch.go
+++ swatch/swatch.go
@@ -1,2 +1,25 @@
 package swatch
 
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2/widget"
+	apptype "github.com/c0nfsd/pixDraw/appType"
+)
+
+type Swatch struct {
+	widget.BaseWidget
+	Selected     bool
+	Color        color.Color
+	SwatchIndex  int
+	clickHandler func(s *Swatch)
+}
+
+func (s *Swatch) SetColor(c color.Color) {
+	s.Color = c
+	s.Refresh()
+}
+
+func NewSwatch(state *apptype.State, color color.Color, swatchIndex int, clickHandler func(s *Swatch)) *Swatch {
+
+}
